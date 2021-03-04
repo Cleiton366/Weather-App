b4e080dc8c48f7f43287166315160903c730e345
Cleiton366 <cleiton0050cf@gmail.com>
2021-03-04 00:54:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Disclaimer:
+I do *NOT* own the rights on theses images and icons used on this project.
+The images and icons here are *NOT* my creative product, nor do I intend to sell
+or profit from the creative and intellectual property of another.
+All images and icons used on this project was downloaded on free stock websites,
+you can see the links to every image bellow.
+
+
+# Description:
+A simple app with [OpenWeatherMap API](https://openweathermap.org/api)(no affiliation) to see the weather forecast on any given city.
+
+# Screenshots:
+![109898856-a702e480-7c73-11eb-88c4-16bd06d18665](https://user-images.githubusercontent.com/57187549/109906602-8e98c700-7c7f-11eb-8aef-1288500727ca.png)
+
+![109899220-4b852680-7c74-11eb-8184-d8c83a417844](https://user-images.githubusercontent.com/57187549/109906608-922c4e00-7c7f-11eb-987e-e3120354ee0a.png)
+
+![109899542-cb12f580-7c74-11eb-9293-1f20befb303b](https://user-images.githubusercontent.com/57187549/109906613-93f61180-7c7f-11eb-81ed-14f4e9adda50.png)
+
+![109900142-a1a69980-7c75-11eb-9a5a-e08b57eba830](https://user-images.githubusercontent.com/57187549/109906619-95bfd500-7c7f-11eb-8c50-ce990615576a.png)
+
+![109902123-bdf80580-7c78-11eb-8988-61e9bdaffbb5](https://user-images.githubusercontent.com/57187549/109906624-98bac580-7c7f-11eb-8398-21ee3aeb8bc8.png)
+
+
+Links to Images:
+
+[Street Person Walk](https://pixabay.com/photos/street-person-walk-snow-winter-1209401/)
+
+[Storm Florida Clouds](https://pixabay.com/photos/storm-florida-clouds-nature-426787/)
+
+[Rain Wet Water](https://pixabay.com/photos/rain-wet-water-drip-window-nature-4806609/)
+
+[Lightning Storm Arizona](https://pixabay.com/photos/lightning-storm-arizona-monsoon-1158027/)
+
+[Conifers Dark Fir Trees](https://pixabay.com/photos/conifers-dark-fir-trees-fog-foggy-1836582/)
+
+[Adult Blur Bokeh](https://pixabay.com/photos/adult-blur-bokeh-city-evening-1867665/)
+
+[Clouds Cumulus Sky](https://pixabay.com/photos/clouds-cumulus-sky-nature-dramatic-2329680/)
+
+[Weather Flat Icons](https://www.iconfinder.com/iconsets/weather-flat-14)
---
+++ js/index-script.js
@@ -0,0 +1,115 @@
+var cities = [];
+var apiKey = '9f4199f4cccf3ad96a5041911c19a8d6';
+
+//open moreInfo
+$(document).on('click', '.divCity', function () {
+    var contentId = $(this).attr("id");
+    document.getElementById("moreInfo").style.marginTop = "-990px";
+    document.getElementById("cityList").style.marginTop = "500px";
+    $("#moreInfo").show();
+    $("#divLogo").hide();
+    $("#divSearch").hide();
+    renderInfo(contentId);
+})
+//close moreInfo
+$(document).on("click", ".fa-arrow-left", function () {
+    document.getElementById("cityList").style.marginTop = "-150px";
+    $("#moreInfo").hide();
+    $("#divLogo").show();
+    $("#divSearch").show();
+    renderCities();
+})
+//delete city
+$(document).on("click", ".fa-times", function () {
+    var contentId = $(this).attr("id");
+    cities.splice(contentId, 1);
+    renderCities();
+    console.log("City " + contentId + " removed");
+})
+$(document).on("keypress", function (e) {
+    if (e.which == 13) {
+        addCity();
+        document.getElementById("searchCity").value = "";
+    }
+})
+
+function addCity() {
+    if (cities.length == 5) {
+        alert("You can only add 5 Cities for now.");
+        document.getElementById("searchCity").value = "";
+        return;
+    }
+    if (document.getElementById("searchCity").value == "") {
+        alert("No one has ever named a city with no name, you must type a valid city name >:[");
+    } else {
+        var cityName = document.getElementById("searchCity").value;
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
+
+        $.getJSON(url, function (data) {
+            cities.push(data);
+            console.log("city added to the grid: "+cityName);
+            renderCities();
+        })
+        document.getElementById("searchCity").value = "";
+    }
+}
+
+function renderCities() {
+    console.log("function called: renderCities");
+    var render = "";
+    var iconUrl = "";
+
+    for (var i = 0; i < cities.length; i++) {
+        console.log("indice:"+i);       
+        iconUrl = "http://openweathermap.org/img/w/"+cities[i].weather[0].icon+".png";
+        render = render +
+            "<div id='" + i + "'class='divCity'>" +
+            "<img class='icon' src='"+iconUrl+"'>"+
+            "<h3 id='cityName'>" + cities[i].name + "</h3>" +
+            "<h3 id='cityTemperature'>" + cities[i].main.temp + " °C" + "</h3>" +
+            "</div>";
+    }
+    document.getElementById("cityList").innerHTML = render;
+}
+
+//render moreInfo Div
+
+function renderInfo(id) {
+    console.log("function called: renderInfo");
+    var iconUrl = "http://openweathermap.org/img/w/"+cities[id].weather[0].icon+".png";
+    var render =
+        "<div id='moreInfo'>" +
+        "<i class='fas fa-arrow-left'></i>" +
+        "<img class='moreInfoIcon' src='"+iconUrl+"'>" +
+        "<h3 class='cityName'>" + cities[id].name + "</h3>" +
+        "<h4 class='cityTemp'>Current: " + cities[id].main.temp + " °C</h4>" +
+        "<h4 class='cityMin'>Min: " + cities[id].main.temp_min + " °C</h4>" +
+        "<h4 class='cityMax'>Max: " + cities[id].main.temp_max + " °C</h4>" +       
+        "<h4 class='cityFeelsLike'>Feels like: " + cities[id].main.feels_like+ " °C</h4>" +
+        "<h4 class='cityHumidity'>Humidity: "+cities[id].main.humidity+"% </h4>"+
+        "<h4 class='cityDescription'>"+cities[id].weather[0].description+"</h4>"+
+        "</div>";
+
+    document.getElementById("moreInfo").innerHTML = render;
+    changeBackground(id);
+}
+
+//background change function
+
+function changeBackground(id){
+    console.log("function called: changeBackground");
+    var temp = cities[id].weather[0].description;
+    if(temp === "snow"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/street-1209401_1920.jpg')";
+    }else if(temp === "clear sky" || temp === "few clouds" || temp === "scattered clouds" || temp === "broken clouds"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/clouds-2329680_1920.jpg')";
+    }else if(temp === "overcast clouds"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/storm-426787_1920.jpg')";
+    }else if (temp === "shower rain" || temp === "rain"|| temp === "light rain" || temp === "moderate rain"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/rain-4806609_1920.jpg')";
+    }else if(temp === "mist" || temp == "haze"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/conifers-1836582_1920.jpg')";
+    }else if(temp === "thunderstorm"){
+        document.body.style.backgroundImage = "url('../weather-app/assets/backgrounds/lightning-1158027_1920.jpg')";
+    }
+}
